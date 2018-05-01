142edda0f0cfbfd64c3dd3fad6662c19c168c43b
kauboy26 <kaushik.gc98@gmail.com>
2018-05-01 17:45:43 -0400

    rewrote lexer to make it more state machine like (somewhat). cleaned a little

--- lex_test_script.py
+++ lex_test_script.py
@@ -1,9 +1,10 @@
 from py_lex_test import MyLexer
 from py_parser_test import MyParser
 
-in_str = '8 + 8 * (8)'
+in_str = '18 + 8 * 9 - (1 - 1) - 9 * 3'
 
 lexer = MyLexer(in_str)
+
 parser = MyParser()
 
 print 'Result:', parser.parse(lexer.get_token_list())
--- py_lex_test.py
+++ py_lex_test.py
@@ -1,3 +1,21 @@
+def is_numeric(c):
+    return c >= '0' and c <= '9'
+
+def is_alpha(c):
+    return c >= 'A' and c <= 'Z' and\
+        c >= 'a' and c <= 'a'
+
+def is_alphanumeric(c):
+    return c >= 'A' and c <= 'Z' and\
+        c >= 'a' and c <= 'a' and\
+        c >= '0' and c <= '9'
+
+def is_special(c):
+    return c == '*' or c == '/' or\
+        c == '+' or c == '-' or\
+        c == '(' or c == ')'
+
+
 # have a lexer class that rips tokens out and gives me one at a time
 class MyLexer():
     def __init__(self, in_str='26'):
@@ -18,31 +36,37 @@ class MyLexer():
     def _process_line(self):
         # actually go through a line until you hit the new line character
         # add things into the token list
-        i = 0
-        num = 0 # use this guy for number tokens
-        processing_num = False
+
         if not self.in_str:
             self.token_list.append(('EOF', None))
             return
 
-        for i in xrange(len(self.in_str)):
-            if self.in_str[i] >= '0' and self.in_str[i] <= '9':
-                processing_num = True
-                num = num * 10 + int(self.in_str[i])
-            else:
-                if processing_num:
-                    # If it was collecting a number until this point, put it
-                    # into the tuple and send it.
-                    self.token_list.append(('NUM', num))
-                    processing_num = False
-                    num = 0
-                if self.in_str[i] == ' ' or self.in_str[i] == '\t':
-                    continue
+        length = len(self.in_str)
+        i = 0
+        while i < length:
+            if is_numeric(self.in_str[i]):
+                # It is a number, grab the entire number.
+                num = 0
+                while i < length and is_numeric(self.in_str[i]):
+                    num = num * 10 + int(self.in_str[i])
+                    i = i + 1
 
+                self.token_list.append(('NUM', num))
+            
+            elif is_alpha(self.in_str[i]):
+                # Requires variables to start with a letter. After that anything is fine
+                # other than operands and spaces
+                start_index = i
+                while i < length and is_alphanumeric(self.in_str[i]):
+                    i = i + 1
+                # At this point, i points to the first next non-alphanumeric character
+                self.token_list.append(('VAR', self.in_str[start_index:i]))
+            elif is_special(self.in_str[i]):
                 self.token_list.append((self.in_str[i], None))
-
-        if processing_num:
-            self.token_list.append(('NUM', num))
+                i = i + 1
+            else:
+                # Eat up whitespace and other junk
+                i = i + 1
 
         self.token_list.append(('EOF', None))
         self.in_str = None
