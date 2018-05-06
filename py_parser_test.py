a810bc41c246f7f705cd407c769f5d7f6f4d08c2
kauboy26 <kaushik.gc98@gmail.com>
2018-05-05 20:03:41 -0400

    cleaned up right paren thing.

--- py_parser_test.py
+++ py_parser_test.py
@@ -8,7 +8,7 @@ class MyParser():
         self.precedence =\
             {'*': 50, '/': 50, '%':50, '+': 40, '-': 40,
             'sin': 60, 'sqrt': 60, 'pow': 60,   # include special functions like these
-            '(': 0, ')':0, '=': 10,
+            '(': 0, ')':1, '=': 10,
             'EOL':0}
 
         # denotes the number of arguments needed when that function is performed.
@@ -59,63 +59,6 @@ class MyParser():
                     op_stack.append(tk_type)
                     i = i + 1
                     continue
-
-                if tk_type == ')':
-                    while op_stack and op_stack[-1] != '(':
-                        operation = op_stack.pop()
-                        if len(num_stack) < self.args_needed[operation]:
-                            print 'Not enough arguments!'
-                            return None
-
-
-                        # See note 1 from notes.md, for why '=' is being tested
-                        # here an not in perform_operation
-                        if operation == '=':
-                            val2 = num_stack.pop()
-                            val1 = num_stack.pop()
-                            if type(val1) == str:
-                                if type(val2) == str:
-                                    if val2 in self.variables:
-                                        self.variables[val1] = self.variables[val2]
-                                        return self.variables[val2]
-                                    else:
-                                        print 'The variable', val2, 'has not been defined.'
-                                        return None
-                                else:
-                                    self.variables[val1] = val2
-                                    return val2
-                            else:
-                                print 'Cannot assign a value to a non-variable.'
-                                return None
-
-
-                        # Now for other operations, pass in a list of operand values
-                        operands = []
-                        for j in xrange(self.args_needed[operation]):
-                            oper = num_stack.pop()
-                            if type(oper) == str:
-                                if oper in self.variables:
-                                    # The operands are being passed in reverse order
-                                    operands.append(self.variables[oper])
-                                else:
-                                    print 'The variable', oper, 'has not been defined.'
-                                    return None
-                            else:
-                                operands.append(oper)
-                        
-                        was_success, new_val = self.perform_operation(operands, operation)
-
-                        if not was_success:
-                            return None
-                        num_stack.append(new_val)
-
-                    if not op_stack:
-                        print 'Mismatched parens.'
-                        return None
-                    else:
-                        op_stack.pop()
-                        i = i + 1
-                        continue
 
                 while op_stack and precedence[tk_type] <= precedence[op_stack[-1]]:
                     # When Ilooking at the current operand token,
@@ -170,8 +113,15 @@ class MyParser():
                     num_stack.append(new_val)
 
                 # At this point, either the operand stack is empty, or the top most
-                # operand has a precedence lower than the newest operand
-                op_stack.append(tk_type)
+                # operand has a precedence lower than the newest operand.
+                if tk_type == ')':
+                    # The thing we stopped on must be '(', otherwise mismatch
+                    if not op_stack or op_stack[-1] != '(':
+                        print 'Mismatched parens.'
+                        return None
+                    op_stack.pop()
+                else:
+                    op_stack.append(tk_type)
             i = i + 1
 
         if num_stack:
